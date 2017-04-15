ebba830d09ae4d01585abaa3d00e3d03cbde6f1d
Lino <luizgustavolino@gmail.com>
2017-04-15 01:31:18 -0300

    implementação da estrutura da mlp

--- MultilayerPerceptron/MLP.py
+++ MultilayerPerceptron/MLP.py
@@ -2,49 +2,63 @@
 # -*- coding: utf8 -*-
 
 import random
+import math
 
 class MLP (object):
 
-    def __init__(self, layout):
+    def __init__(self, layout, expected = None):
         # cria uma rede MLP seguindo a quantidade
         # de neuronios do layout (vetor de ints)
 
         print "Criando MLP, layout:" + str(layout)
 
-        self.layers = []
-        layersCount = len(layout)
+        self.layers  = [] # layers[0]  -> input layer, demais são hidden layers
+        self.outputs = []
+        layersCount  = len(layout)
 
         # Criando os layers, inputs,neuronios e outputs
+        # enumerate([2,2,1]) -> [(0,2),(1,2),(2,1)]
         for i, nCount in enumerate(layout):
             currentLayer = []
             for j in range(0, nCount):
                 if i == 0:
-                    print "Criando Neuronio "+str(j)+" da input layer"
+                    print "Criando neuronio "+str(j)+" da input layer"
                     tag = "iln"+str(j)
                     currentLayer.append(Input(tag))
-                elif i < layersCount-1:
-                    print "Criando Neuronio "+str(j)+" da layer "+str(i)
+                elif i < layersCount:
+                    print "Criando neuronio "+str(j)+" da layer "+str(i)
                     tag = "hl"+str(i)+"n"+str(j)
                     currentLayer.append(Neuron(tag))
-                else:
-                    print "Criando Neuronio "+str(j)+" da output layer"
-                    tag = "oln"+str(j)
-                    currentLayer.append(Output(tag))
+
             self.layers.append(currentLayer)
 
         # Criando as sinapses entre os neuronios
         for i, layer in enumerate(self.layers):
-            if i == layersCount - 1: continue
-            nextLayer = self.layers[i+1]
-            for neuron in layer:
-                for target in nextLayer:
-                    synapse = Synapse(neuron,target)
-                    neuron.synapsesOut.append(synapse)
-                    target.synapsesIn.append(synapse)
+
+            if i < layersCount - 1:
+                nextLayer = self.layers[i+1]
+                for neuron in layer:
+                    for target in nextLayer:
+                        synapse = Synapse(neuron,target)
+                        neuron.synapsesOut.append(synapse)
+                        target.synapsesIn.append(synapse)
+            else:
+                for neuron in layer:
+                    out = Output("output", neuron)
+                    neuron.synapsesOut.append(out)
+                    self.outputs.append(out)
 
     def forward(self, inputs):
-        # Avisa as sinapses de entrada e espera a saida
+
+        # Avisa as sinapses de entrada e,
+        # após os eventos em cascata, colhe a saida
+        print "-------------------------"
         print "Iniciando passo forward"
+        print "-------------------------"
+        for (j, inputNode) in enumerate(self.layers[0]):
+            inputNode.receiveSignal(inputs[j])
+
+        # resultado esta no vetor de outputs
 
 class Input (object):
 
@@ -55,14 +69,25 @@ class Input (object):
         self.tag         = tag
         self.synapsesOut = []
 
+    def receiveSignal(self, signalValue):
+        # a input layer não tem somatório de valores
+        # ela recebe de uma fonte somente, então para já ativar
+        # o próximo passo
+        for synapse in self.synapsesOut: synapse.signal(signalValue)
+
 class Output (object):
 
-    def __init__(self, tag):
+    def __init__(self, tag, source):
         # Outputs são o resultado do processamento
         # representam uma classe na classificação
         # Não tem sinapseOut, só sinapseIn
-        self.tag = tag
+        # também não tem peso, recebe sinal integralmente
+        self.source     = source
         self.synapsesIn = []
+        self.tag        = tag
+
+    def signal(self, signalValue):
+        print("Output: " + str(signalValue))
 
 class Neuron (object):
 
@@ -72,19 +97,32 @@ class Neuron (object):
         self.tag                = tag
         self.signalsReceived    = 0
         self.accumulatedWeight  = 0.0
-        self.bias               = Bias()
-        self.synapsesIn         = []
+        self.bias               = Bias(self)
+        self.synapsesIn         = [self.bias]
         self.synapsesOut        = []
 
-    def receiveSignal(self, signalValue):
-        print("Recebendo sinal no neuronio " + self.tag)
+    def receiveSignal(self, signalValue, fromTag):
+
+        print(" (" + fromTag + ") Recebendo da sinapse sinal: " + str(signalValue))
         self.signalsReceived += 1
         self.accumulatedWeight += signalValue
-        if self.signalsReceived == len(self.sinapseIn) -1:
-            self.accumulatedWeight +=
-            print("Neuronio saturado " + self.tag)
 
-    def sigmoid():
+        if self.signalsReceived == len(self.synapsesIn) -1:
+            # quando n-1 neurônios entragaram o sinal
+            # significa que só falta o bias
+            self.bias.signal()
+
+        elif self.signalsReceived == len(self.synapsesIn):
+            # todos sinais foram recebidos e somados!
+            # agora é só calcular a sigmoid e passar adiante
+            # também é preciso resetar para se preparar
+            sigmoidData = self.sigmoid()
+            print("# " + self.tag + " saturado com sigmoid " + str(sigmoidData))
+            for synapse in self.synapsesOut: synapse.signal(sigmoidData)
+            self.accumulatedWeight = 0
+            self.signalsReceived   = 0
+
+    def sigmoid(self):
         return 1 / (1+math.exp(self.accumulatedWeight))
 
 class Synapse (object):
@@ -92,21 +130,26 @@ class Synapse (object):
     def __init__(self, source, destiny):
         # cria uma sinapse, que tem um neuronio
         # de entrada e um de saida
-        self.src = source
-        self.dst = destiny
+        self.source = source
+        self.destiny = destiny
         self.weight = random.uniform(-1,1)
         print "Sinapse criada entre "+source.tag+ " e "+destiny.tag
 
     def signal(self,input):
         # recebe um estimulo e repassa
         # para outro neuronio
-        self.dst.receiveSignal(self.weight * input)
+        self.destiny.receiveSignal(self.weight * input, self.source.tag +" -> "+ self.destiny.tag)
 
 class Bias (object):
 
-    def __init__(self):
+    def __init__(self, destiny):
         # similar a Sinapse, mas sempre tem
         # 1 como entrada
-        pass
+        self.destiny = destiny
+        self.weight = random.uniform(-1,1)
 
-MLP([2,1,1])
+    def signal(self):
+        self.destiny.receiveSignal(self.weight * 1, "bias")
+
+trab = MLP([4,8,5,3])
+trab.forward([0.4, 0.1, 0.4, 0.9])
